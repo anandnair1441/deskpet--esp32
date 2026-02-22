b3fd7088aa3144013acb581d5c24b0c2d141c63b
Anand Nair <anandnair1441@gmail.com>
2026-02-22 14:49:25 +0530

    blinking

--- src/main.cpp
+++ src/main.cpp
@@ -19,15 +19,51 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+unsigned long lastBlink_time=0;
+unsigned long Blinkstart_time=0;
+unsigned long timeNow;
+long interval=3500;
+long duration=150;
+int isBlinking=0;
+
+
+
 void drawEyes() {
-    display.fillRoundRect(EYE_X_L, EYE_Y, BASE_EYE_W, EYE_H, EYE_RADIUS, SSD1306_WHITE);
-    display.fillRoundRect(EYE_X_R, EYE_Y, BASE_EYE_W, EYE_H, EYE_RADIUS, SSD1306_WHITE);
+    if(isBlinking==1){
+        display.fillRoundRect(EYE_X_L, EYE_Y, BASE_EYE_W, EYE_H, EYE_RADIUS, SSD1306_WHITE);
+        display.fillRoundRect(EYE_X_L, EYE_Y-5, BASE_EYE_W, EYE_H, EYE_RADIUS, SSD1306_BLACK);
+
+        display.fillRoundRect(EYE_X_R, EYE_Y, BASE_EYE_W, EYE_H, EYE_RADIUS, SSD1306_WHITE);
+        display.fillRoundRect(EYE_X_R, EYE_Y-5, BASE_EYE_W, EYE_H, EYE_RADIUS, SSD1306_BLACK);
+    }
+    if(isBlinking==0){
+        display.fillRoundRect(EYE_X_L, EYE_Y, BASE_EYE_W, EYE_H, EYE_RADIUS, SSD1306_WHITE);
+        display.fillRoundRect(EYE_X_R, EYE_Y, BASE_EYE_W, EYE_H, EYE_RADIUS, SSD1306_WHITE);
+    }
 }
 
 void drawMouth() {
-    // Smile: big circle with top cut off by black circle
+    // big circle with top cut off by black circle
     display.fillCircle(64, MOUTH_Y + 5, 9, SSD1306_WHITE);
     display.fillCircle(64, MOUTH_Y + 1, 9, SSD1306_BLACK);
+}
+
+void updateBlink(){
+    timeNow=millis();
+    
+    if(!isBlinking && timeNow-lastBlink_time>interval){
+        isBlinking=1;
+        Blinkstart_time=timeNow;
+    }
+    
+    if(isBlinking && timeNow-Blinkstart_time>duration){
+        isBlinking=0;
+        lastBlink_time=timeNow;
+
+        interval=random(3500,7000);
+        duration=random(100,200);
+    }
+    
 }
 
 void setup() {
@@ -41,6 +77,7 @@ void setup() {
 }
 
 void loop() {
+    updateBlink();
     display.clearDisplay();
     drawEyes();
     drawMouth();
